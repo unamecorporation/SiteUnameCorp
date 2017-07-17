34769677db3d88f6c4291d1b2a93ab9ade06a9df
guiluigi <guiluigi1712@gmail.com>
2017-07-17 20:49:39 -0300

    finalização da home

--- js/init.js
+++ js/init.js
@@ -3,3 +3,21 @@ $( document ).ready(function(){
     $('.tooltipped').tooltip({delay: 50});
     $('.parallax').parallax();
 })
+
+var texto = "Uname Corp.";
+var result;
+
+var count = 0;
+function digitar() {
+  result = document.getElementById("logo");
+  window.setTimeout(function() { inserir(texto[count]) }, 200);
+}
+
+function inserir(letra) {
+  result.innerHTML += letra;
+  count++;
+  if(count < texto.length)
+    window.setTimeout(function() { inserir(texto[count]) }, 200);
+}
+
+window.onload = digitar;
